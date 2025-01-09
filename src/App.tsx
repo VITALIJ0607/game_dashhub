f22ed3553fe36c8d16cd36a29da421b20cad2520
Vitalij Zavjalov <vitalijzavjalov@gmail.com>
2025-01-09 01:03:05 +0100

    Add navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <div></div>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider value={0}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </React.StrictMode>
